e23b21e51d972409106dd4f399482478cbf6bad0
Mark Heramis <mark@dentalray.healthcare>
2024-02-12 01:18:01 +0800

    add volume mounts, fix authorization startup and replace Token header to Authorization Header
    Signed-off-by: Mark Heramis <mark@dentalray.healthcare>

--- express-server/server.js
+++ express-server/server.js
@@ -1,37 +1,28 @@
 "use strict";
-
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
-
 // parse application/json
 app.use(bodyParser.json());
-
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
-
-app.post("/auth", (req, res) => {
-  console.log("Received request:");
-  console.dir(req.body);
-  const authRequest = req.body;
-  const authResponse = {
-    granted: true,
-    validity: 0,
-  };
-
-  // TODO: use dicom-uid or parse data from uri and restrict user access
-  if (authRequest["token-value"] != "demo") {
-    authResponse.granted = false;
-    console.log("[FAIL] operation not allowed!");
-  } else {
-    console.log("[OK] operation allowed!");
-    console.log("\n --------------------------- \n");
-  }
-
-  res.send(JSON.stringify(authResponse));
+app.post("/auth", (request, response) => {
+	const request_body = request.body;
+	const response_body = {
+		granted: true,
+		validity: 0,
+	};
+	// TODO: use dicom-uid or parse data from uri and restrict user access
+	if (request_body["token-value"] != "demo") {
+		response_body.granted = false;
+		console.log("[FAIL] operation not allowed!");
+	} else {
+		console.log("[OK] operation allowed!");
+		console.log("\n --------------------------- \n");
+	}
+	response.send(JSON.stringify(response_body));
 });
-
 app.listen(PORT, HOST, () => {
-  console.log(`[OK] Running on http://${HOST}:${PORT}`);
+	console.log(`[OK] Running on http://${HOST}:${PORT}`);
 });
