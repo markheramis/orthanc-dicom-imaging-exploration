359b0929c698a33b1073eeddf8b28fdbd571d337
Mark Clu Heramis <chumheramis@gmail.com>
2024-03-08 04:12:23 +0800

    Update server.js
    updated server.js /auth endpoint so that it will now check if the `token-value` exists in the headers.
    
    if it doesn't we return an error.
    
    it it does, we strip the "bearer " part of it and continue with the logic.

--- express-server/server.js
+++ express-server/server.js
@@ -8,21 +8,33 @@ app.use(bodyParser.json());
 const PORT = 8080;
 const HOST = "0.0.0.0";
 app.post("/auth", (request, response) => {
-	const request_body = request.body;
-	const response_body = {
-		granted: true,
-		validity: 0,
-	};
-	// TODO: use dicom-uid or parse data from uri and restrict user access
-	if (request_body["token-value"] != "demo") {
-		response_body.granted = false;
-		console.log("[FAIL] operation not allowed!");
-	} else {
-		console.log("[OK] operation allowed!");
-		console.log("\n --------------------------- \n");
-	}
-	response.send(JSON.stringify(response_body));
+    const request_body = request.body;
+    const response_body = {
+        granted: true,
+        validity: 0,
+    };
+    // Check if token exists
+    if (!request_body.hasOwnProperty("token-value")) {
+        response_body.granted = false;
+        console.log("[FAIL] No token provided!");
+        response.send(JSON.stringify(response_body));
+        return;
+    }
+    let token = request_body["token-value"];
+    // Remove "bearer " part of the token
+    if (token.startsWith("bearer ")) {
+        token = token.slice(7);
+    }
+	// check if token is "demo"
+    if (token != "demo") {
+        response_body.granted = false;
+        console.log("[FAIL] operation not allowed!");
+    } else {
+        console.log("[OK] operation allowed!");
+        console.log("\n --------------------------- \n");
+    }
+    response.send(JSON.stringify(response_body));
 });
 app.listen(PORT, HOST, () => {
-	console.log(`[OK] Running on http://${HOST}:${PORT}`);
+    console.log(`[OK] Running on http://${HOST}:${PORT}`);
 });
